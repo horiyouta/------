fcd6d11c130685d54f226a27ec3dd3506b643e6a
horiyouta <109045913+horiyouta@users.noreply.github.com>
2024-01-28 18:29:40 +0900

    1/28 18:29

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ const tool = /** @type { HTMLParagraphElement } */ (document.getElementById(`too
 const info = /** @type { HTMLParagraphElement } */ (document.getElementById(`info`));
 const sInp = /** @type { HTMLInputElement } */ (document.getElementById(`sInp`));
 const nsg = /** @type { HTMLDivElement } */ (document.getElementById(`nsg`));
+const mycanvas = /** @type { HTMLCanvasElement } */ (document.createElement(`canvas`));
+const input = /** @type { HTMLInputElement } */ (document.getElementById(`input`));
+const video = /** @type { HTMLVideoElement } */ (document.createElement(`video`));
+const ta = /** @type { HTMLTextAreaElement } */ (document.getElementById(`ta`));
+const maxSize = 75;
 
 // 変数
 
@@ -80,6 +85,32 @@ const convertRound = () => {
     }
 }
 
+const changeInput = () => {
+    video.src = URL.createObjectURL(new Blob([input.files[0]], { type: `video/mp4` }));
+    video.addEventListener(`loadeddata`, () => {
+        if (video.videoHeight < video.videoWidth) {
+            mycanvas.width = maxSize;
+            mycanvas.height = video.videoHeight * maxSize / video.videoWidth;
+        } else {
+            mycanvas.width = video.videoWidth * maxSize / video.videoHeight;
+            mycanvas.height = maxSize;
+        }
+        const ctx = /** @type { CanvasRenderingContext2D } */ (mycanvas.getContext(`2d`));
+        let text = `${mycanvas.width}/${mycanvas.height}/`;
+        const func = () => {
+            ctx.drawImage(video, 0, 0, mycanvas.width, mycanvas.height);
+            const imageData = ctx.getImageData(0, 0, mycanvas.width, mycanvas.height).data;
+            for (let i = 0; i < imageData.length; i += 4) {
+                text += imageData[i].toString(16).padStart(2, `0`) + imageData[i + 1].toString(16).padStart(2, `0`) + imageData[i + 2].toString(16).padStart(2, `0`);
+            }
+            if (!video.paused) setTimeout(func, 1000 / 15);
+            else ta.value = text;
+        }
+        video.play();
+        func();
+    });
+}
+
 // 全体処理
 
 frameFunc();
@@ -90,3 +121,4 @@ topPage.addEventListener(`click`, () => { mode = 0 });
 tool.addEventListener(`click`, () => { mode = 2 });
 info.addEventListener(`click`, () => { mode = 3 });
 convert.addEventListener(`click`, convertRound);
+input.addEventListener(`change`, changeInput);
